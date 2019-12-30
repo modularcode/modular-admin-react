b22a8a5c16e2696605f9c9fd010aa6741995acaf
Gevorg Harutyunyan <gevharut@gmail.com>
2019-12-30 16:51:11 +0400

    Adjust storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,16 +1,20 @@
 import 'storybook-chromatic'
-
 import React from 'react'
 import { configure, addDecorator, addParameters } from '@storybook/react'
 import { withA11y } from '@storybook/addon-a11y'
+import { themes } from '@storybook/theming';
 import { ThemeProvider } from '@material-ui/styles'
-import theme from '../src/_theme'
+
+
+import docsTheme from './theme'
+import appTheme from '../src/_theme'
 
 // import { GlobalStyle } from '../src/components/shared/global';
 
 addParameters({
   options: {
     showRoots: true,
+    theme: docsTheme,
     storySort: (a, b) => {
       const aNestingLevel = a[1].parameters.fileName.split('/').length
       const bNestingLevel = b[1].parameters.fileName.split('/').length
@@ -23,7 +27,7 @@ addParameters({
 addDecorator(withA11y);
 addDecorator(story => (
   <>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={appTheme}>
       {story()}
     </ThemeProvider>
   </>
---
+++ .storybook/theme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create';
+import logo from '../public/logo-storybook.png';
+
+
+export default create({
+  base: 'light',
+  brandTitle: 'Modular Material Admin + React',
+//   brandUrl: 'https://modular-admin.com',
+  brandImage: logo,
+});
