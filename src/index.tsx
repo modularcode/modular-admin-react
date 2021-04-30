df3dd8f88d13e63a46be76361b21ffe3cdac9bbc
Gevorg Harutyunyan <gevharut@gmail.com>
2021-05-01 02:18:15 +0400

    Fix app crashing

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import 'typeface-roboto'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
 
 import authService from './_services/authService'
 import api from './_api/'
@@ -19,8 +18,3 @@ import api from './_api/'
 
   ReactDOM.render(<App />, document.getElementById('root'))
 })()
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
